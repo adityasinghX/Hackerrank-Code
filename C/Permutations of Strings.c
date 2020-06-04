a5fe56a306f4f267a746e974b59b8cfe61807e0a
adityasinghX <36784231+adityasinghX@users.noreply.github.com>
2020-06-04 19:09:03 +0530

    Add files via upload

---
+++ C/Permutations of Strings.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int next_permutation(int n, char **s)
+{
+	/**
+	* Complete this method
+	* Return 0 when there is no next permutation and 1 otherwise
+	* Modify array s to its next permutation
+	*/
+}
+
+int main()
