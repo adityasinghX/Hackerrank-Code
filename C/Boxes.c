191def6e841f82eaf78b9b14575ff5061a858167
adityasinghX <36784231+adityasinghX@users.noreply.github.com>
2020-06-09 12:01:15 +0530

    Add files via upload

---
+++ C/Boxes.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_HEIGHT 41
+
+struct box
+{
+    int length, width, height;
+	/**
+	* Define three fields of type int: length, width and height
+	*/
+};
+
+typedef struct box box;
+
+int get_volume(box b) {
+    return b.length*b.width*b.height;
+	/**
+	* Return the volume of the box
+	*/
+}
+
+int is_lower_than_max_height(box b) {
+    return (b.height <41) ? 1 :0 ;
+	/**
+	* Return 1 if the box's height is lower than MAX_HEIGHT and 0 otherwise
+	*/
+}
+
+int main()
